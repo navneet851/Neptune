16c3aba4d628511083eb8347a143306bf8c74f64
navneet <nav700neet@gmail.com>
2024-02-16 21:23:37 +0530

    Album screen

--- app/src/main/java/com/music/stream/neptune/screens/AlbumScreen.kt
+++ app/src/main/java/com/music/stream/neptune/screens/AlbumScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -34,7 +33,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -42,7 +40,6 @@ import com.music.stream.neptune.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
 fun AlbumScreen(navController: NavController) {
     Scaffold(
